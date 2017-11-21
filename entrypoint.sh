ee8d10733fc386be049753b7529fc8d8cc6c671e
jonathan.striebel <jstriebel@users.noreply.github.com>
2017-11-21 13:12:56 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,30 +3,30 @@ set -e
 
 USER_UID=${USER_UID:-1000}
 USER_GID=${USER_GID:-1000}
-SBT_USER=${SBT_USER:-sbt}
+USER_NAME=${USER_NAME:-sbt}
 
-if [[ -d "/home/${SBT_USER}" ]]; then
-  chown ${USER_UID}:${USER_GID} "/home/${SBT_USER}"
+if [[ -d "/home/${USER_NAME}" ]]; then
+  chown ${USER_UID}:${USER_GID} "/home/${USER_NAME}"
   # copy user files from /etc/skel
-  install -g ${USER_GID} -o ${USER_UID} /etc/skel/* "/home/${SBT_USER}"
+  install -g ${USER_GID} -o ${USER_UID} /etc/skel/* "/home/${USER_NAME}"
 fi
 
 # create group with USER_GID
-if ! getent group "${SBT_USER}" >/dev/null; then
-  groupadd -f -g ${USER_GID} "${SBT_USER}" 2> /dev/null
+if ! getent group "${USER_NAME}" >/dev/null; then
+  groupadd -f -g ${USER_GID} "${USER_NAME}" 2> /dev/null
 fi
 
 # create user with USER_UID
-if ! getent passwd "${SBT_USER}" >/dev/null; then
+if ! getent passwd "${USER_NAME}" >/dev/null; then
   adduser --disabled-login --uid ${USER_UID} --gid ${USER_GID} \
-    --gecos "${SBT_USER}" "${SBT_USER}"
+    --gecos "${USER_NAME}" "${USER_NAME}"
 fi
 
 if [ "$1" = 'sbt' ] || [ "$1" = 'bash' ] || [ "$1" = 'sh' ]; then
-    cd "/home/${SBT_USER}"
-    exec sudo -HEu "${SBT_USER}" $@
+    cd "/home/${USER_NAME}"
+    exec sudo -HEu "${USER_NAME}" $@
 else
-    su ${SBT_USER}" << EOF
+    su ${USER_NAME}" << EOF
     exec sbt $@
 EOF
 fi
